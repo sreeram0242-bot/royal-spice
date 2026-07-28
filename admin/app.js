1167864bfda8610397ac72749eb00455f10757f7
sree ram <sreeram02420@gmail.com>
2026-07-28 10:20:19 +0530

    loadfix

--- admin/app.js
+++ admin/app.js
@@ -13,7 +13,7 @@ function showLoader() {
   if (!loader) {
     loader = document.createElement('div');
     loader.id = 'globalLoader';
-    loader.style.cssText = 'position:fixed;top:0;left:0;width:100%;height:100%;background:var(--bg-color, #121212);display:flex;align-items:center;justify-content:center;z-index:9999;';
+    loader.style.cssText = 'position:fixed;top:0;left:0;width:100%;height:100%;background:var(--bg-dark, #121212);display:flex;align-items:center;justify-content:center;z-index:9999;';
     loader.innerHTML = `
       <style>
         .loader-container { display: flex; justify-content: center; align-items: center; }
@@ -67,7 +67,7 @@ function showLoader() {
   if (!loader) {
     loader = document.createElement('div');
     loader.id = 'globalLoader';
-    loader.style.cssText = 'position:fixed;top:0;left:0;width:100%;height:100%;background:var(--bg-color, #121212);display:flex;align-items:center;justify-content:center;z-index:9999;';
+    loader.style.cssText = 'position:fixed;top:0;left:0;width:100%;height:100%;background:var(--bg-dark, #121212);display:flex;align-items:center;justify-content:center;z-index:9999;';
     loader.innerHTML = `
       <style>
         .loader-container { display: flex; justify-content: center; align-items: center; }
